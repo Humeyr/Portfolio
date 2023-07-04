f20e590af8dd6cdc486af51c85742f9272251224
Humeyr <humeyraydogdu@outlook.com.tr>
2023-07-04 19:10:35 +0200

    updated

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
 export const graphcms = new GraphQLClient(
-    "https://app.hygraph.com/5a90f2dfb23a4b099f73111c916c995d/master/content/01062c5fb64f49ea96434bd9a62873b9/view/30a581e87669443592aface98277ac28",
+    "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clh33vuv54q2e01ta8p58cwvc/master",
     {
         headers:{},
     }
